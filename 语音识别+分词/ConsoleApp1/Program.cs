2c79329eb21027e4e351c82049163bd4f05b7457
chenqi <chenqi@smartiot.cc>
2018-01-31 12:11:19 +0800

    项目初始化

---
+++ 语音识别+分词/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JiebaNet.Analyser;
+using JiebaNet.Segmenter;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test1();
+            Test2();
+
+            Console.ReadLine();
+        }
+
+        static void Test1()
+        {
+            var segmenter = new JiebaSegmenter();
+            var segments = segmenter.Cut("我来到北京清华大学", cutAll: true);
+            Console.WriteLine("【全模式】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.Cut("我来到北京清华大学");  // 默认为精确模式
+            Console.WriteLine("【精确模式】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.Cut("他来到了网易杭研大厦");  // 默认为精确模式，同时也使用HMM模型
+            Console.WriteLine("【新词识别】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.CutForSearch("小明硕士毕业于中国科学院计算所，后在日本京都大学深造"); // 搜索引擎模式
+            Console.WriteLine("【搜索引擎模式】：{0}", string.Join("/ ", segments));
+
+            segments = segmenter.Cut("结过婚的和尚未结过婚的");
+            Console.WriteLine("【歧义消除】：{0}", string.Join("/ ", segments));
+        }
+
+        static void Test2()
+        {
+            JiebaSegmenter seg = new JiebaSegmenter();
+            seg.AddWord("风机");
+            seg.AddWord("风机1");
+            seg.AddWord("风机2");
+            seg.AddWord("风机3");
+            seg.AddWord("启动");
+            seg.AddWord("停止");
+            seg.AddWord("开");
+            seg.AddWord("关");
+            seg.AddWord("停");
+            seg.AddWord("停止");
+            seg.AddWord("风");
+            
+            //var segments = seg.Cut("风机1启动");  // 默认为精确模式
+            //Console.WriteLine("【精确模式】：{0}", string.Join("/ ", segments));
+
+            Console.WriteLine($"{string.Join("/",seg.Cut("风机1启动"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("风机1开"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("风机1关"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("风机1停"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("启动风机1"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("开风机1"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("关风机"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("停止风机1"))}");
+            Console.WriteLine($"{string.Join("/", seg.Cut("开风机4"))}");
+        }
+    }
+}
---
+++ 语音识别+分词/语音识别DEMO/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 语音识别DEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //App ID  10663392
+            //API Key oKyF9Oz9GK8RKUZgR8Gf86uk
+            //Secret Key DBYeFMm5r8Vwaj6toInoUT4KFE6GXt1a
+
+            // 设置APPID/AK/SK
+            var APP_ID = "10663392";
+            var API_KEY = "oKyF9Oz9GK8RKUZgR8Gf86uk";
+            var SECRET_KEY = "DBYeFMm5r8Vwaj6toInoUT4KFE6GXt1a";
+
+            var client = new Baidu.Aip.Speech.Asr(API_KEY, SECRET_KEY);
+            //var data = File.ReadAllBytes("16k.pcm");
+            //var result = client.Recognize(data, "pcm", 16000);
+            //var data = File.ReadAllBytes("16k.wav");
+            //var result = client.Recognize(data, "wav", 16000);
+            //Console.Write(result);
+
+            string path = "16k.wav";
+            string format = "wav";
+            M4a2pcm(path, "wav","test.wav");
+            var data = File.ReadAllBytes("test.wav");
+            var result = client.Recognize(data, format, 16000);
+            Console.Write(result);
+
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 转码为pcm
+        /// </summary>
+        /// <param name="path"></param>
+        static void M4a2pcm(string path, string format, string newFileName)
+        {
+            string Command = " -i \"" + path + "\" -vn  -f wav -c:a pcm_s16le -ar 16000 -ac 1 " + newFileName;
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = @"tool\ffmpeg.exe";
+            p.StartInfo.Arguments = Command;
+            p.StartInfo.WorkingDirectory = "bin/tools/";
+
+            #region 方法一  
+            //p.StartInfo.UseShellExecute = false;//不使用操作系统外壳程序 启动 线程  
+            //p.StartInfo.RedirectStandardError = true;//把外部程序错误输出写到StandardError流中(这个一定要注意,FFMPEG的所有输出信息,都为错误输出流,用 StandardOutput是捕获不到任何消息的...  
+            //p.StartInfo.CreateNoWindow = false;//不创建进程窗口  
+            //p.Start();//启动线程  
+            //p.BeginErrorReadLine();//开始异步读取  
+            //p.WaitForExit();//等待完成  
+            //p.Close();//关闭进程  
+            //p.Dispose();//释放资源  
+            #endregion
+
+            #region 方法二  
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            p.Start();//启动线程  
+            p.WaitForExit();//等待完成  
+            p.Close();//关闭进程  
+            p.Dispose();//释放资源  
+            #endregion
+        }
+
+    }
+}
